60b7ea7335e65a281a4a133bbe1124d0e3563364
Kevin Haro <artcom_3@hotmail.com>
2020-07-19 00:52:33 -0500

    Add TimePicker

--- app/src/main/java/com/example/time2/NotificationActivity.java
+++ app/src/main/java/com/example/time2/NotificationActivity.java
@@ -5,6 +5,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
@@ -13,6 +14,8 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TimePicker;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Calendar;
@@ -21,8 +24,11 @@ public class NotificationActivity extends AppCompatActivity {
 
     private EditText editHours, editMins;
     Button saveNotifications;
+    ImageButton btn;
     private String CHANNEL_NAME = "Default Channel";
     private String CHANNEL_ID = "com.example.time2" + CHANNEL_NAME;
+
+    int mHour, mMin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,28 @@ public class NotificationActivity extends AppCompatActivity {
         editHours = findViewById(R.id.editHour);
         editMins = findViewById(R.id.editMinute);
         saveNotifications = findViewById(R.id.button_saveNotification);
+
+        btn = findViewById(R.id.imageButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar c = Calendar.getInstance();
+                mHour = c.get(Calendar.HOUR_OF_DAY);
+                mMin = c.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(NotificationActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String sHour = String.valueOf(hourOfDay);
+                        String sMin = String.valueOf(minute);
+                        editHours.setText(sHour);
+                        editMins.setText(sMin);
+                    }
+                }, mHour, mMin, true);
+                timePickerDialog.show();
+            }
+        });
 
         saveNotifications.setOnClickListener(new View.OnClickListener() {
             @Override
